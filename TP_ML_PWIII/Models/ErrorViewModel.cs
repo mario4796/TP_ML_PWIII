0efca2d6e4481b70f09c2259d77aa8b8c1be9a83
Mario <mario.checchia@outlook.com>
2025-10-26 20:52:40 -0300

    Add project files.

---
+++ TP_ML_PWIII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TP_ML_PWIII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
