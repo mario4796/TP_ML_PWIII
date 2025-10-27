4814d7713924215b5aa9739dd0379fa6f3f4e053
Mario <mario.checchia@outlook.com>
2025-10-26 22:07:56 -0300

    se corrigio un problema con los colores

--- TP_ML_PWIII/Controllers/HomeController.cs
+++ TP_ML_PWIII/Controllers/HomeController.cs
@@ -6,24 +6,16 @@ namespace TP_ML_PWIII.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-
         public IActionResult Acerca()
         {
             return View();
         }
-
 
         public IActionResult Explorar()
         {
@@ -33,17 +25,6 @@ namespace TP_ML_PWIII.Controllers
         public IActionResult Perfil()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
